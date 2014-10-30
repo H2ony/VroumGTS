90ff7996ab2224d985311798b1b2e51a2e40baf4
Makinomaro <julien.angeloni@yahoo.com>
2014-10-30 11:03:30 +0100

    Create Ecurie.java

---
+++ Ecurie.java
@@ -0,0 +1,16 @@
+public class Ecurie {
+	
+	String nom;
+	Voiture voiture1;
+	Voiture voiture2;
+	int point;
+	
+	public Ecurie(String nom, Voiture voiture1, Voiture voiture2, int point) {
+		
+		this.nom = nom;
+		this.voiture1 = voiture1;
+		this.voiture2 = voiture2;
+		this.point = point;
+	}
+	
+}
