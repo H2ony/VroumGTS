77148281cc5f9734c771dc2dd65056f63fd4800a
Makinomaro <julien.angeloni@yahoo.com>
2014-10-30 11:04:58 +0100

    Create GrandPrix.java

---
+++ GrandPrix.java
@@ -0,0 +1,15 @@
+public class GrandPrix {
+	
+	String nom;
+	int nbrTour;
+	Tour classGP[];
+	
+	public GrandPrix(String nom, int nbrTour, Tour classGP[]) {
+	
+		this.nom = nom;
+		this.nbrTour = nbrTour;
+		this.classGP = classGP;
+		
+	}
+
+}
