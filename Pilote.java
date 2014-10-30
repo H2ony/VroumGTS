4881b80a6b785d37ca044daa2ec51276060dbaa7
Makinomaro <julien.angeloni@yahoo.com>
2014-10-30 11:01:12 +0100

    Create Pilote.java

---
+++ Pilote.java
@@ -0,0 +1,13 @@
+public class Pilote { 	 	
+  
+  String nom; 	
+  String prenom; 	
+  int point; 	 	
+  
+  public Pilote(String nom, String prenom, int point) { 		 		
+    this.nom = nom; 		
+    this.prenom = prenom; 		
+    this.point = point; 	
+    } 	 
+
+}
