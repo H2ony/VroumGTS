2bf228ae3db609f980a0e7838d7702d333cc5fc8
H2ony <hoyetanthony@gmail.com>
2014-10-31 09:13:45 +0100

    Modification de Tour
    j'ai enlevé le paramètre que tu passais car on peut l'initialiser dans
    la classe directement et j'ai mis la méthode ajouter tour. il faudra
    qu'on numérote les voitures de 1 a 20 pour manipuler le tableau plus
    facilement.

--- Tour.java
+++ Tour.java
@@ -2,10 +2,14 @@ public class Tour {
 
 	Voiture classTour[];
 	
-	public Tour(Voiture classTour[]) {
+	public Tour() {
 		
-		this.classTour = classTour;
+		this.classTour = new Voiture[20];
 	}
+        
+        public void ajouterTour(int numeroVoiture, int tour){
+            this.classTour[numeroVoiture-1]= tour;
+        }
 	
 }
 	
